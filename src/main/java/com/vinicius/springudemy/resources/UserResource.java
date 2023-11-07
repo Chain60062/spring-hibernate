c4b3a67b29361686349678a1527a866a3efbf946
chain60062 <viniciusmmenezes0@gmail.com>
2023-11-07 10:55:51 -0300

    Feita criação de User.

--- src/main/java/com/vinicius/springudemy/resources/UserResource.java
+++ src/main/java/com/vinicius/springudemy/resources/UserResource.java
@@ -1,12 +1,16 @@
 package com.vinicius.springudemy.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vinicius.springudemy.entities.User;
 import com.vinicius.springudemy.services.UserService;
@@ -21,13 +25,22 @@ public class UserResource {
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<User>> findAll() {
         List<User> users = service.findAll();
         return ResponseEntity.ok().body(users);
     }
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
+    public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
